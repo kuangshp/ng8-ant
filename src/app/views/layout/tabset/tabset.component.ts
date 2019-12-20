4b6b4d466c2c5f27b62120ded025619029e28ad7
kuangshp <kuangshp@126.com>
2019-12-20 15:43:57 +0800

    导航菜单

--- src/app/views/layout/tabset/tabset.component.ts
+++ src/app/views/layout/tabset/tabset.component.ts
@@ -11,6 +11,23 @@ export class TabsetComponent implements OnInit {
   constructor () { }
 
   ngOnInit() {
+    if (this.activeTabs.length === 0) {
+      this.activeTabs.push({
+        id: '0',
+        index: 0,
+        enabled: true,
+        name: '首页',
+        close: false,
+        icon: 'home',
+        refresh: 'N',
+        content: {
+          ID: '0',
+          MENUID: 'HOME',
+          ROUTER: 'home',
+          MENUTYPE: 'INURL'
+        }
+      })
+    }
   }
 
   // 选择菜单切换路由
