38c781a9d0d2efa20fe7f8b91ead5c976b77fd0d
kuangshp <kuangshp@126.com>
2019-12-20 16:07:49 +0800

    删除组件siderbar

--- src/app/views/layout/layout.module.ts
+++ src/app/views/layout/layout.module.ts
@@ -4,13 +4,12 @@ import { SharedModule } from '@app/shared/shared.module';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { LayoutComponent } from './layout.component';
 import { TabsetComponent } from './tabset/tabset.component';
 
 
 @NgModule({
-  declarations: [LayoutComponent, HeaderComponent, SidebarComponent, TabsetComponent],
+  declarations: [LayoutComponent, HeaderComponent, TabsetComponent],
   imports: [
     LayoutRoutingModule,
     SharedModule,
