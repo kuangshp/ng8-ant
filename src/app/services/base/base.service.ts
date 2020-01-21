6b48f4d26b1153dab13edcc0a240b84ab103e540
kuangshp <kuangshp@126.com>
2020-01-21 22:44:36 +0800

    重命名文件夹名

--- src/app/services/base/base.service.ts
+++ src/app/services/base/base.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from './../../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 interface IobjectType {
   [propsName: string]: any;
